f274c375aacb4294b82f6247b1647b2e3a204439
howeixiong <howeixiong@users.noreply.github.com>
2018-04-24 01:31:38 +0800

    Add files via upload
    Rewrote Round Robin algorithm

--- simulator.py
+++ simulator.py
@@ -51,61 +51,47 @@ def FCFS_scheduling(process_list):
 #Output_1 : Schedule list contains pairs of (time_stamp, process_id) indicating the time switching to that process_id
 #Output_2 : Average Waiting Time
 def RR_scheduling(process_list, time_quantum ):
-    schedule = []
-    ready_queue = []
-    to_insert_into_queue = None
+    process_index = 0
     current_time = 0
-    waiting_time = 0
+    ready_queue = []
     last_scheduled_id = -1
-    for process in process_list:
-        if (current_time < process.arrive_time):
-            time_to_go = process.arrive_time - current_time
-            while (time_to_go > 0):
-                if (len(ready_queue) == 0):
-                    current_time = process.arrive_time
-                    print "Fast forward to time %d" % current_time
-                    last_scheduled_id = -1
-                    break
-                process_id, quantum, burst = ready_queue[0]
-                if (process_id != last_scheduled_id):
-                    schedule.append((current_time,process_id))
-                    last_scheduled_id = process_id
-                while True:
-                    print "Ready queue is: "
-                    print ready_queue
-                    print "Schedule %d at time %d" % (process_id, current_time)
-                    ready_queue[0][1] = quantum = quantum - 1
-                    ready_queue[0][2] = burst = burst - 1
-                    time_to_go = time_to_go - 1
-                    current_time = current_time + 1
-                    waiting_time += len(ready_queue) - 1
-                    if burst == 0:
-                        ready_queue.pop(0)
-                        break
-                    if quantum == 0:
-                        ready_queue.pop(0)
-                        to_insert_into_queue = [process_id, time_quantum, burst]
-                        break
-                    if time_to_go == 0:
-                        break
-        ready_queue.append([process.id, time_quantum, process.burst_time])
-        if (to_insert_into_queue is not None):
-            ready_queue.append(to_insert_into_queue)
-            to_insert_into_queue = None
-        print "Ready queue is: "
-        print ready_queue
-    # code easier now that only two values left
-    while (len(ready_queue) > 0):
-        process_id, quantum, burst = ready_queue[0]
-        if (process_id != last_scheduled_id):
-            schedule.append((current_time,process_id))
-            last_scheduled_id = process_id
-        jump_time = min(quantum, burst)
-        current_time += jump_time
-        ready_queue.pop(0)
-        waiting_time += len(ready_queue) * jump_time
-        if burst > jump_time:
-            ready_queue.append([process_id, time_quantum, burst - jump_time])
+    schedule = []
+    waiting_time = 0
+
+    while (process_index < len(process_list)) or (len(ready_queue) > 0): # while there is something to do
+        if process_index < len(process_list):
+            new_process = process_list[process_index]
+            
+            # append process to queue if it has arrived
+            if new_process.arrive_time == current_time:
+                ready_queue.append( [new_process.id, new_process.burst, time_quantum, new_process.arrive_time + new_process.burst] )
+                process_index += 1
+          
+        # what should I do at this current time?
+        if len(ready_queue) == 0:
+            # nothing to do -> move forward one time unit (for optimisation, can fast forward to next process' arrival time)
+            current_time += 1
+            last_scheduled_id = -1
+        else:
+            # do first process in the ready queue
+            process_id, process_burst, process_quantum, process_earliest_end_time = ready_queue.pop(0)
+            
+            if last_scheduled_id != process.id:
+              schedule.append( [current_time, process_id] )
+              last_scheduled_id = process.id
+            
+            # increment time
+            process.burst -= 1
+            process.quantum -= 1
+            current_time += 1
+            
+            if process.burst == 0:
+                # done with this process
+                waiting_time += current_time - process_earliest_end_time
+            elif process.quantum == 0:
+                # this process has used up its quantum -> move it to the back of the queue
+                ready_queue.append( [process)id, process_burst, time_quantum, process_earliest_end_time] )
+    
     average_waiting_time = waiting_time/float(len(process_list))
     return schedule, average_waiting_time
 
